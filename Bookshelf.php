5aec2e6ec180da3319b4cb6bc8f2c3a6ca34bc8f
pwirth <wirth@hallowelt.biz>
2020-01-31 09:05:45 +0100

    Use new hook handler base class for hook BSUEModulePDFcollectMetaData
    ERM:10378
    [3.2]
    
    Change-Id: Ibb00e974158a073ae1d467481d6d2f09f8c6e5be

--- Bookshelf.php
+++ Bookshelf.php
@@ -60,36 +60,7 @@ class Bookshelf extends BsExtensionMW {
 		$this->setHook( 'BSUEModulePDFgetPage' );
 		$this->setHook( 'BSUEModulePDFbeforeGetPage' );
 
-		$this->setHook( 'BSUEModulePDFcollectMetaData' );
 		$this->setHook( 'BSUEModulePDFAfterFindFiles' );
-	}
-
-	/**
-	 *
-	 * @param Title $oTitle
-	 * @param DOMDocument $oPageDOM
-	 * @param array &$aParams
-	 * @param DOMXPath $oDOMXPath
-	 * @param array &$aMeta
-	 * @return bool Always true to keep hook running
-	 */
-	public function onBSUEModulePDFcollectMetaData( $oTitle, $oPageDOM, &$aParams, $oDOMXPath,
-			&$aMeta ) {
-		if ( $oTitle->getNamespace() != NS_BOOK ) {
-			return true;
-		}
-
-		if ( $this->getConfig()->get( 'BookshelfSupressBookNS' ) ) {
-			// Otherwise it has intentionally been overwritten and we don't want to overwrite it
-			// again
-			if ( $aMeta['title'] === $oTitle->getPrefixedText() ) {
-			 $aMeta['title'] = $oTitle->getText();
-			}
-		}
-		// TODO RBV (01.02.12 14:14): Currently the bs:bookmeta tag renders a
-		// div.bs-universalexport-meta. Therefore things like "subtitle" are
-		// read in by BsPDFPageProvider. Not sure if this is good...
-		return true;
 	}
 
 	/**
