7850a92d91ba6219550025a070f65e7f4cad7e1e
rvogel <vogel@hallowelt.biz>
2020-12-11 12:11:01 +0100

    Enable translation for `Book` namespace
    [3.1.x]
    
    ERM22175
    
    Change-Id: Ia85aa07d358991434d1345ebe6a58f0f1b92378d

--- languages/Bookshelf.namespaces.php
+++ languages/Bookshelf.namespaces.php
@@ -1,21 +1,34 @@
 <?php
 
 /**
- * Translations of the namespaces introduced by Blog.
+ * Translations of the namespaces introduced by Bookshelf.
  *
  * @file
  */
 
 $namespaceNames = [];
 
-/** English */
 $namespaceNames['en'] = [
-	NS_BOOK => 'Book',
-	NS_BOOK_TALK => 'Book_talk',
+	1504 => 'Book',
+	1505 => 'Book_talk'
 ];
 
-/** German */
 $namespaceNames['de'] = [
-	NS_BOOK => 'Buch',
-	NS_BOOK_TALK => 'Buch_Diskussion',
+	1504 => 'Buch',
+	1505 => 'Buch_Diskussion'
+];
+
+$namespaceNames['es'] = [
+	1504 => 'Libro',
+	1505 => 'Libro_discusión'
+];
+
+$namespaceNames['fr'] = [
+	1504 => 'Livre',
+	1505 => 'Discussion_livre'
+];
+
+$namespaceNames['it'] = [
+	1504 => 'Libro',
+	1505 => 'Discussioni_libro'
 ];
