8a3d2781d98539d9e88475c345ad5da21e5aea75
dvogel <daniel.vogel@hallowelt.com>
2020-12-11 10:12:13 +0100

    Add a chapter pager for books
    [3.2]
    
    ERM22129
    
    Change-Id: I81812cc69791064b781688dcbe3f2a054d72ecf3

---
+++ src/ConfigDefinition/ChapterPagerBeforeContent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlueSpice\Bookshelf\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition\BooleanSetting;
+
+class ChapterPagerBeforeContent extends BooleanSetting implements \BlueSpice\Bookshelf\ISettingPaths {
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_BOOK . '/BlueSpiceBookshelf',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceBookshelf/' . static::FEATURE_BOOK,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_PRO . '/BlueSpiceBookshelf',
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-bookshelf-pref-chapterpagerbeforecontent';
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHelpMessageKey() {
+		return 'bs-bookshelf-pref-chapterpagerbeforecontent-help';
+	}
+}
