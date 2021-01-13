652066d5d3a992478b5e4e182f4f93a5f99fff13
pwirth <wirth@hallowelt.biz>
2021-01-13 13:03:42 +0100

    Add missing deprecation to legacy hook BSBookshelfBookUI
    ERM:10378
    [3.3]
    
    Change-Id: Ibe2e00484526c0a2071b54e6a6dba53e9a0010f8

--- src/Hook/BSBookshelfGetBookData/RunLegacyHook.php
+++ src/Hook/BSBookshelfGetBookData/RunLegacyHook.php
@@ -4,11 +4,31 @@ namespace BlueSpice\Bookshelf\Hook\BSBookshelfGetBookData;
 
 use BlueSpice\Bookshelf\Hook\BSBookshelfGetBookData;
 
+/**
+ * @deprecated since version 3.3 - use hook 'BSBookshelfGetBookData' instead
+ */
 class RunLegacyHook extends BSBookshelfGetBookData {
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		return empty( $GLOBALS['wgHooks']['BSBookshelfBookUI'] );
+	}
 
+	/**
+	 * @deprecated since version 3.3 - use hook 'BSBookshelfGetBookData' instead
+	 */
 	protected function doProcess() {
-		$this->getServices()->getHookContainer()->run( 'BSBookshelfBookUI', [
-			null, $this->getContext()->getOutput(), $this->bookData
-		] );
+		wfDebugLog( 'bluespice-deprecations', __METHOD__, 'private' );
+		$this->getServices()->getHookContainer()->run(
+			'BSBookshelfBookUI',
+			[
+				null,
+				$this->getContext()->getOutput(),
+				$this->bookData
+			],
+			[ 'deprecatedVersion' => '3.3' ]
+		);
 	}
 }
