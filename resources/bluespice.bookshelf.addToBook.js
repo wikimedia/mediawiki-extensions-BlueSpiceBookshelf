176d3983872871d66f27e63015011f4a39ca1c0a
dvogel <daniel.vogel@hallowelt.com>
2021-04-20 12:28:27 +0200

    Use dialog instead of window for edit dialog
    [3.2]
    
    ERM17733
    
    Change-Id: I121e7da02670eec9955b9e698a3ab4096d2739fd

--- resources/bluespice.bookshelf.addToBook.js
+++ resources/bluespice.bookshelf.addToBook.js
@@ -2,7 +2,9 @@ $(document).on( 'click', '#ca-bookshelf-add-to-book', function( e ) {
 	mw.loader.using( 'ext.bluespice.extjs' ).done( function() {
 		Ext.require( 'BS.Bookshelf.dialog.AddToBook', function() {
 			var dlg = new BS.Bookshelf.dialog.AddToBook();
-			dlg.setData( mw.config.get( 'wgPageName' ) );
+			dlg.setData( {
+				'pagename': mw.config.get( 'wgPageName' )
+			} );
 			dlg.show();
 		});
 	});
