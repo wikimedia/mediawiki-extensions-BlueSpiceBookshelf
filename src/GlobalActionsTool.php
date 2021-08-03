e1a324f284b308f6f1584f2672148bea9e889390
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-03 14:51:38 +0200

    Add extension to SkinSlot GlobalActions and MainLinkPanel
    [4.x]
    
    Change-Id: I645515af768b9be7c2fa1bc5117401bf708ce4b8

---
+++ src/GlobalActionsTool.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BlueSpice\Bookshelf;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsTool extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-bookshelf';
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'bookshelf-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$specialPage = SpecialPage::getTitleFor( 'BookshelfBookManager' );
+		return $specialPage->getLocalURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bookshelfbookmanager' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-bookshelfui-extension-description' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bookshelfbookmanager' );
+	}
+}
---
+++ src/MainLinkPanel.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BlueSpice\Bookshelf;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class MainLinkPanel extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'n-bookshelf';
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'bookshelf-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$specialPage = SpecialPage::getTitleFor( 'Bookshelf' );
+		return $specialPage->getLocalURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bookshelf' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bookshelf' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bookshelf' );
+	}
+
+}
