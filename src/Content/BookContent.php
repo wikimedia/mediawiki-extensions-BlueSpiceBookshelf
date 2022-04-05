04bc816813757ccae317ce6caa4532163f277c38
dvogel <daniel.vogel@hallowelt.com>
2022-04-05 12:55:39 +0200

    Make templatelinks work for content model book and roll back to base class TextContent
    [3.2.x]
    
    ERM28243
    
    Change-Id: Ic85c6265051ac202c4c711496c4d5375cd019df5

--- src/Content/BookContent.php
+++ src/Content/BookContent.php
@@ -3,21 +3,21 @@
 namespace BlueSpice\Bookshelf\Content;
 
 use BlueSpice\Bookshelf\BookEditData;
+use MediaWiki\MediaWikiServices;
 use MWException;
 use ParserOptions;
 use ParserOutput;
+use TextContent;
 use Title;
-use WikitextContent;
 
-class BookContent extends WikitextContent {
+class BookContent extends TextContent {
 	/**
 	 * @param string $text
 	 * @param string $model_id
 	 * @throws MWException
 	 */
 	public function __construct( $text, $model_id = 'book' ) {
-		parent::__construct( $text );
-		$this->model_id = $model_id;
+		parent::__construct( $text, $model_id );
 	}
 
 	/**
@@ -30,6 +30,10 @@ class BookContent extends WikitextContent {
 	protected function fillParserOutput(
 		Title $title, $revId, ParserOptions $options, $generateHtml, ParserOutput &$output
 	) {
+		// parse just to get links etc into the database.
+		$services = MediaWikiServices::getInstance();
+		$parser = $services->getService( 'Parser' );
+		$output = $parser->parse( $this->getNativeData(), $title, $options, true, true, $revId );
 		try {
 			$bookEditData = BookEditData::newFromTitleAndRequest(
 				$title, new \WebRequest()
