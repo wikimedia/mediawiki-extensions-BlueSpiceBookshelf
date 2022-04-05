06b024acec32fd3cda68ab232beb69e8ec270486
dvogel <daniel.vogel@hallowelt.com>
2022-04-05 12:55:39 +0200

    Make templatelinks work for content model book and roll back to base class TextContent
    [3.2.x]
    
    ERM28243
    
    Change-Id: Ic85c6265051ac202c4c711496c4d5375cd019df5

--- src/ContentHandler/BookContentHandler.php
+++ src/ContentHandler/BookContentHandler.php
@@ -5,14 +5,14 @@ namespace BlueSpice\Bookshelf\ContentHandler;
 use BlueSpice\Bookshelf\Action\BookEditAction;
 use BlueSpice\Bookshelf\Action\BookEditSourceAction;
 use BlueSpice\Bookshelf\Content\BookContent;
-use WikitextContentHandler;
+use TextContentHandler;
 
-class BookContentHandler extends WikitextContentHandler {
+class BookContentHandler extends TextContentHandler {
 	/**
 	 * @param string $modelId
 	 */
 	public function __construct( $modelId = 'book' ) {
-		parent::__construct( $modelId );
+		parent::__construct( $modelId, [ CONTENT_FORMAT_TEXT ] );
 	}
 
 	/**
