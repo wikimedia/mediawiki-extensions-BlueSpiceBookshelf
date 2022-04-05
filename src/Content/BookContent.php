13e34e1acca80c6e42728dc823d69f6eec7685f6
dvogel <daniel.vogel@hallowelt.com>
2022-04-05 12:55:39 +0200

    Make templatelinks work for content model book and roll back to base class TextContent
    [3.2.x]
    
    ERM28243
    
    Change-Id: Ic85c6265051ac202c4c711496c4d5375cd019df5

--- src/Content/BookContent.php
+++ src/Content/BookContent.php
@@ -2,44 +2,16 @@
 
 namespace BlueSpice\Bookshelf\Content;
 
-use BlueSpice\Bookshelf\BookEditData;
 use MWException;
-use ParserOptions;
-use ParserOutput;
-use Title;
-use WikitextContent;
+use TextContent;
 
-class BookContent extends WikitextContent {
+class BookContent extends TextContent {
 	/**
 	 * @param string $text
 	 * @param string $model_id
 	 * @throws MWException
 	 */
 	public function __construct( $text, $model_id = 'book' ) {
-		parent::__construct( $text );
-		$this->model_id = $model_id;
-	}
-
-	/**
-	 * @param Title $title
-	 * @param int $revId
-	 * @param ParserOptions $options
-	 * @param bool $generateHtml
-	 * @param ParserOutput &$output
-	 */
-	protected function fillParserOutput(
-		Title $title, $revId, ParserOptions $options, $generateHtml, ParserOutput &$output
-	) {
-		try {
-			$bookEditData = BookEditData::newFromTitleAndRequest(
-				$title, new \WebRequest()
-			);
-			$output->addJsConfigVars( 'bsBookshelfData', $bookEditData->getBookData() );
-			$output->addModules( [ 'ext.bluespice.bookshelf.view' ] );
-			$output->setText( \Html::element( 'div', [ 'id' => 'bs-bookshelf-view' ] ) );
-		}
-		catch ( MWException $e ) {
-			$output->addWarningMsg( "bs-bookshelf-warning", $e->getText() );
-		}
+		parent::__construct( $text, $model_id );
 	}
 }
--- src/ContentHandler/BookContentHandler.php
+++ src/ContentHandler/BookContentHandler.php
@@ -4,15 +4,22 @@ namespace BlueSpice\Bookshelf\ContentHandler;
 
 use BlueSpice\Bookshelf\Action\BookEditAction;
 use BlueSpice\Bookshelf\Action\BookEditSourceAction;
+use BlueSpice\Bookshelf\BookEditData;
 use BlueSpice\Bookshelf\Content\BookContent;
-use WikitextContentHandler;
+use Content;
+use MediaWiki\Content\Renderer\ContentParseParams;
+use MediaWiki\MediaWikiServices;
+use MWException;
+use ParserOutput;
+use TextContentHandler;
+use Title;
 
-class BookContentHandler extends WikitextContentHandler {
+class BookContentHandler extends TextContentHandler {
 	/**
 	 * @param string $modelId
 	 */
 	public function __construct( $modelId = 'book' ) {
-		parent::__construct( $modelId );
+		parent::__construct( $modelId, [ CONTENT_FORMAT_TEXT ] );
 	}
 
 	/**
@@ -32,4 +39,39 @@ class BookContentHandler extends WikitextContentHandler {
 		];
 	}
 
+	/**
+	 * @param Content $content
+	 * @param ContentParseParams $cpoParams
+	 * @param ParserOutput &$output The output object to fill (reference).
+	 */
+	protected function fillParserOutput(
+		Content $content,
+		ContentParseParams $cpoParams,
+		ParserOutput &$output
+	) {
+		// parse just to get links etc into the database, HTML is replaced below.
+		$output = MediaWikiServices::getInstance()->getParser()
+			->parse(
+				$content->getText(),
+				$cpoParams->getPage(),
+				$cpoParams->getParserOptions(),
+				true,
+				true,
+				$cpoParams->getRevId()
+			);
+
+		try {
+			$dbKey = $cpoParams->getPage()->getDBkey();
+			$title = Title::newFromDBkey( $dbKey );
+			$bookEditData = BookEditData::newFromTitleAndRequest(
+				$title, new \WebRequest()
+			);
+			$output->addJsConfigVars( 'bsBookshelfData', $bookEditData->getBookData() );
+			$output->addModules( [ 'ext.bluespice.bookshelf.view' ] );
+			$output->setText( \Html::element( 'div', [ 'id' => 'bs-bookshelf-view' ] ) );
+		}
+		catch ( MWException $e ) {
+			$output->addWarningMsg( "bs-bookshelf-warning", $e->getText() );
+		}
+	}
 }
