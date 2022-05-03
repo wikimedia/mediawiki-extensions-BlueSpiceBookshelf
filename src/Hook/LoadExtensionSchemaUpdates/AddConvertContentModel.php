d8c9ac9c22869cf1290452bc870eee84704af7ae
Klein Muçi <kleinmuci@gmail.com>
2022-05-03 22:55:09 +0000

    Fix typo
    Bug: T201491
    Change-Id: Iea1f722acd58ee3fe490e8b7552accd052811f63

--- src/Hook/LoadExtensionSchemaUpdates/AddConvertContentModel.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddConvertContentModel.php
@@ -6,7 +6,7 @@ use BlueSpice\Hook\LoadExtensionSchemaUpdates;
 
 class AddConvertContentModel extends LoadExtensionSchemaUpdates {
 	protected function doProcess() {
-		// UserBookContentLanguage must be executed before teh conversion to book content model!
+		// UserBookContentLanguage must be executed before the conversion to book content model!
 		$this->updater->addPostDatabaseUpdateMaintenance( 'UserBookContentLanguage' );
 		$this->updater->addPostDatabaseUpdateMaintenance(
 			'ConvertContentModel'
