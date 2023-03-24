b5f7c650171243e1c56ab82cbf6d463b9d5286f0
miriam schlindwein <schlindwein@hallowelt.com>
2023-03-24 15:46:37 +0100

    Merge RL modules
    ERM31645
    
    [4.x]
    
    Change-Id: I170927745abde0ef6017a6b1db05a5b95163f8ef

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -5,9 +5,6 @@ namespace BlueSpice\Bookshelf\Hook\BeforePageDisplay;
 class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
-		$this->out->addModules( 'ext.bluespice.bookshelf.addToBook' );
-
-		$this->out->addModuleStyles( 'ext.bluespice.bookshelf.ui.styles' );
 		$this->out->addModules( 'ext.bluespice.bookshelf.navigationTab' );
 
 		if ( !$this->skin->getUser()->isAnon() ) {
@@ -20,17 +17,6 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 				$this->skin->getUser()->getUserPage()->getNsText()
 					. ':' . $location->inContentLanguage()->parse()
 			);
-		}
-		$config = $this->getConfig();
-		$pagerBeforeContent = $config->get( 'BookShelfShowChapterNavigationPagerBeforeContent' );
-		$pagerAfterContent = $config->get( 'BookShelfShowChapterNavigationPagerAfterContent' );
-
-		if ( ( $pagerBeforeContent === true ) || ( $pagerBeforeContent === 1 ) ) {
-			$this->out->addModuleStyles( 'ext.bluespice.bookshelf.pager.before-content.styles' );
-		}
-
-		if ( ( $pagerAfterContent === true ) || ( $pagerAfterContent === 1 ) ) {
-			$this->out->addModuleStyles( 'ext.bluespice.bookshelf.pager.after-content.styles' );
 		}
 	}
 
