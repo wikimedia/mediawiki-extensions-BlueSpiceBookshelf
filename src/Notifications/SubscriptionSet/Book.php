37739f7d0726736b5dbd1c221c59a86457164223
d.savuljesku <d.savuljesku@gmail.com>
2023-10-20 13:56:21 +0200

    Add Book subscriptionSet for Notifications
    [4.x]
    
    ERM30580
    
    Change-Id: Ie5ae9354f6041f21e67352cfb51aa8a047af9095
    (cherry picked from commit 81d4c3340104695f284fda7ce99aae4bf47c1808)

---
+++ src/Notifications/SubscriptionSet/Book.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BlueSpice\Bookshelf\Notifications\SubscriptionSet;
+
+use BlueSpice\Bookshelf\BookLookup;
+use MediaWiki\Extension\Notifications\SubscriberProvider\ManualProvider\ISubscriptionSet;
+use MediaWiki\User\UserIdentity;
+use MWStake\MediaWiki\Component\Events\INotificationEvent;
+use MWStake\MediaWiki\Component\Events\TitleEvent;
+use TitleFactory;
+
+class Book implements ISubscriptionSet {
+	/** @var BookLookup */
+	private $bookLookup;
+
+	/** @var TitleFactory */
+	private $titleFactory;
+
+	/**
+	 * @param BookLookup $bookLookup
+	 * @param TitleFactory $titleFactory
+	 */
+	public function __construct( BookLookup $bookLookup, TitleFactory $titleFactory ) {
+		$this->titleFactory = $titleFactory;
+		$this->bookLookup = $bookLookup;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function isSubscribed( array $setData, INotificationEvent $event, UserIdentity $user ): bool {
+		if ( !( $event instanceof TitleEvent ) ) {
+			return false;
+		}
+		$book = $setData['book'] ?? null;
+		if ( !$book ) {
+			return false;
+		}
+		$requestedBookTitle = $this->titleFactory->makeTitleSafe( NS_BOOK, $book );
+		if ( !$requestedBookTitle || !$requestedBookTitle->exists() ) {
+			return false;
+		}
+		$books = array_keys( $this->bookLookup->getBooksForPage( $event->getTitle() ) );
+		return in_array( $requestedBookTitle->getPrefixedDBkey(), $books );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getClientSideModule(): string {
+		return "ext.bluespice.bookshelf.notificationsSubscriptionSet";
+	}
+}
