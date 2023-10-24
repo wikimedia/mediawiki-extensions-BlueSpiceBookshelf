b5d00e67b64093dedf90c9b1217cf79be398ef04
hslater <hamish.slater@hallowelt.com>
2023-10-24 18:33:04 +1300

    Implement Book Navigation droplet
    ERM33692
    
    [REL1_39, master]
    
    Change-Id: I4a9b895fa99be273516fef258ae0795e2d641f65

---
+++ src/ContentDroplets/BookNavDroplet.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace BlueSpice\Bookshelf\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
+use Message;
+
+class BookNavDroplet extends TagDroplet {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'bs-bookshelf-booknav-name' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return Message::newFromKey( 'bs-bookshelf-booknav-desc' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'booknav-icon';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.bluespice.booknav.visualEditorTagDefinition' ];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCategories(): array {
+		return [ 'content', 'navigation', 'lists' ];
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getTagName(): string {
+		return 'booknav';
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAttributes(): array {
+		return [];
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function hasContent(): bool {
+		return true;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getVeCommand(): ?string {
+		return 'booknavCommand';
+	}
+}
