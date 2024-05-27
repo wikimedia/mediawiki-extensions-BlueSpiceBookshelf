efc705a22c52c84b51e48e9b6a4524209e150774
Mschlindwein <schlindwein@hallowelt.com>
2024-05-27 14:48:01 +0200

    Add metadata action in book overview
    ERM37392
    
    [4.5.x]
    
    Change-Id: Idf8b5796bf88ca9da95ed2aa5519d9dce35a5297

---
+++ src/BooksOverviewActions/BookMetaData.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace BlueSpice\Bookshelf\BooksOverviewActions;
+
+use BlueSpice\Bookshelf\IBooksOverviewAction;
+use Message;
+use Title;
+
+class BookMetaData implements IBooksOverviewAction {
+
+	/**
+	 * @var Title
+	 */
+	private $book = null;
+
+	/**
+	 * @var string
+	 */
+	private $displayTitle = '';
+
+	/**
+	 * @param Title $book
+	 * @param string $displayTitle
+	 */
+	public function __construct( Title $book, string $displayTitle ) {
+		$this->book = $book;
+		$this->displayTitle = $displayTitle;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return 'book_meta';
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPosition(): int {
+		return 60;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getClasses(): array {
+		return [ 'bs-books-overview-action-metadata' ];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIconClasses(): array {
+		return [ 'bi-book' ];
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return new Message( 'bs-books-overview-page-book-action-metadata-text' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		$titleText = $this->book->getPrefixedText();
+		if ( $this->displayTitle !== '' ) {
+			$titleText = $this->displayTitle;
+		}
+		return new Message( 'bs-books-overview-page-book-action-metadata-title', [ $titleText ] );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		return '';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRequiredPermission(): string {
+		return 'edit';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.bluespice.bookshelf.editmetadata' ];
+	}
+}
