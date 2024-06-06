4f95c0e00b387c25b4a3584456e154dd87cf839b
d.savuljesku <d.savuljesku@gmail.com>
2024-06-06 14:07:26 +0200

    Add SearchInBook content droplet
    Depends-On: I05f987e93dc81fbf8c84f1ee1e72e81e73facbd5
    
    [4.5.x]
    
    ERM33699
    
    Change-Id: I3678e9003e9e1cd5e7fb01b61e32026dce795dbb
    (cherry picked from commit fb1b958aeea8b1bc1638759f91a3a67d45e06f85)
    (cherry picked from commit d62c83c6cedda75d7838b943325df5fb42f9ca1f)

---
+++ src/Tag/SearchInBook.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BlueSpice\Bookshelf\Tag;
+
+use BlueSpice\ParamProcessor\IParamDefinition;
+use BlueSpice\ParamProcessor\ParamDefinition;
+use BlueSpice\ParamProcessor\ParamType;
+use BlueSpice\Tag\IHandler;
+use BS\ExtendedSearch\Tag\TagSearch;
+use ConfigException;
+use MediaWiki\MediaWikiServices;
+use Message;
+use Parser;
+use PPFrame;
+
+class SearchInBook extends TagSearch {
+
+	/**
+	 * @param mixed $processedInput
+	 * @param array $processedArgs
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return IHandler
+	 * @throws ConfigException
+	 */
+	public function getHandler(
+		$processedInput,
+		array $processedArgs,
+		Parser $parser,
+		PPFrame $frame
+	) {
+		$config = MediaWikiServices::getInstance()
+			->getConfigFactory()
+			->makeConfig( 'bsg' );
+		$this->tagCounter++;
+
+		return new SearchInBookHandler(
+			$processedInput,
+			$processedArgs,
+			$parser,
+			$frame,
+			$config,
+			$this->tagCounter,
+			MediaWikiServices::getInstance()->getService( 'BSBookshelfBookLookup' )
+		);
+	}
+
+	/**
+	 * @return array|string[]
+	 */
+	public function getTagNames() {
+		return [ 'bs:searchinbook' ];
+	}
+
+	/**
+	 * @return IParamDefinition[]
+	 */
+	public function getArgsDefinitions() {
+		$params = parent::getArgsDefinitions();
+		$params[] = new ParamDefinition(
+			ParamType::STRING,
+			'book',
+			Message::newFromKey( 'bs-bookshelf-tag-search-in-books-book' )->plain()
+		);
+		return $params;
+	}
+}
