dc5abd849794be572620e41412e9d3cefaddc187
Daniel Vogel <daniel.vogel@hallowelt.com>
2024-06-14 13:38:29 +0200

    Add book overview action view
    [4.5.x]
    
    ERM37581
    
    Change-Id: If88ec22d36c28887342961ab29bc6c17f733aaf2

---
+++ src/BooksOverviewActions/View.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace BlueSpice\Bookshelf\BooksOverviewActions;
+
+use BlueSpice\Bookshelf\IBooksOverviewAction;
+use Message;
+use Title;
+
+class View implements IBooksOverviewAction {
+
+	/**
+	 * @var Title
+	 */
+	private $book = null;
+
+	/**
+	 * @var string
+	 */
+	private $displayTitle = '';
+
+	/**
+	 * @param Title $book
+	 * @param string $displayTitle
+	 */
+	public function __construct( Title $book, string $displayTitle ) {
+		$this->book = $book;
+		$this->displayTitle = $displayTitle;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return 'book_view';
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPosition(): int {
+		return 30;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getClasses(): array {
+		return [ 'bs-books-overview-action-view' ];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIconClasses(): array {
+		return [ 'bi-file-earmark' ];
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return new Message( 'bs-books-overview-page-book-action-view-text' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		$titleText = $this->book->getPrefixedText();
+		if ( $this->displayTitle !== '' ) {
+			$titleText = $this->displayTitle;
+		}
+		return new Message( 'bs-books-overview-page-book-action-view-title', [ $titleText ] );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		return $this->book->getLocalUrl();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRequiredPermission(): string {
+		return 'read';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRLModules(): array {
+		return [];
+	}
+}
