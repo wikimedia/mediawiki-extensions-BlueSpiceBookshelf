f348fedb9699304c8c3a6afa0707fda62fc5dcb0
Mschlindwein <schlindwein@hallowelt.com>
2024-09-05 09:53:27 +0200

    Fix subchapters for level more than 3
    Fix unit tests numbering
    
    ERM38532
    
    [4.5.x]
    
    Change-Id: I670ac5f2db81ef91dcf447d7c162d1e13f9b4818
    (cherry picked from commit 23c297142da6ba7d456f2d927a27c260f7b6e836)
    (cherry picked from commit f9ba4eee3c6f8701f3b33f944d4446580da9eeda)

--- src/BookViewTreeDataBuilder.php
+++ src/BookViewTreeDataBuilder.php
@@ -192,14 +192,20 @@ class BookViewTreeDataBuilder {
 			return false;
 		}
 
-		$chapterNumbers = explode( '.', $item->getNumber() );
-		$firstChapterNumber = $chapterNumbers[ 0 ];
+		$parentChapterNumbers = explode( '.', $parent->getNumber() );
+		$childChapterNumbers = explode( '.', $item->getNumber() );
 
-		if ( $firstChapterNumber === $parent->getNumber() ) {
-			return true;
-		} else {
+		if ( count( $childChapterNumbers ) <= count( $parentChapterNumbers ) ) {
 			return false;
 		}
+
+		for ( $i = 0; $i < count( $parentChapterNumbers ); $i++ ) {
+			if ( $parentChapterNumbers[$i] !== $childChapterNumbers[$i] ) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 }
