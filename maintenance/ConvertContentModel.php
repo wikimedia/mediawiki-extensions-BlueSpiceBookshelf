23579802c0613c8172c2848bdaacd8520674f41a
d.savuljesku <d.savuljesku@gmail.com>
2024-09-05 12:04:46 +0200

    Also convert content models of revisions to book CM
    [4.4.x]
    
    ERM37075
    
    Change-Id: I9df865ef0cac7e89635bfc65fb3fca80499f6c08
    (cherry picked from commit 2865cd67cd77abe43a98f3c5cf72821c6083c3d9)
    (cherry picked from commit 9abdf6e3fcfb79f39f6d9f732f456e4aff77cc6a)

--- maintenance/ConvertContentModel.php
+++ maintenance/ConvertContentModel.php
@@ -8,6 +8,11 @@ require_once dirname( dirname( dirname( __DIR__ ) ) ) . '/maintenance/Maintenanc
 class ConvertContentModel extends LoggedUpdateMaintenance {
 
 	/**
+	 * @var int
+	 */
+	private $bookContentModelId;
+
+	/**
 	 * @param IDatabase $db
 	 * @return array
 	 */
@@ -21,14 +26,16 @@ class ConvertContentModel extends LoggedUpdateMaintenance {
 
 		$pages = [];
 		foreach ( $res as $row ) {
-			if ( $row->page_content_model === 'book' ) {
-				// Nothing to do
-				continue;
-			}
 			$title = Title::newFromRow( $row );
 			if ( !$title ) {
 				continue;
 			}
+			if ( $row->page_content_model === 'book' ) {
+				$this->assertRevisionContentModel( $title );
+				// Nothing to do
+				continue;
+			}
+
 			$pages[(int)$row->page_id] = $title;
 		}
 
@@ -57,6 +64,7 @@ class ConvertContentModel extends LoggedUpdateMaintenance {
 		);
 		if ( $res ) {
 			foreach ( $pages as $title ) {
+				$this->convertRevisionContentModel( $title );
 				$title->invalidateCache();
 			}
 		}
@@ -69,6 +77,7 @@ class ConvertContentModel extends LoggedUpdateMaintenance {
 	protected function doDBUpdates() {
 		$this->output( "...Update '" . $this->getUpdateKey() . "': " );
 		$dbLoadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancer();
+		$this->bookContentModelId = $this->getBookContentModelId();
 		$pages = $this->getPages(
 			$dbLoadBalancer->getConnection( DB_REPLICA )
 		);
@@ -86,6 +95,75 @@ class ConvertContentModel extends LoggedUpdateMaintenance {
 	protected function getUpdateKey() {
 		return 'bs_bookmaker_convert_content_model3';
 	}
+
+	/**
+	 * @param Title $title
+	 * @return void
+	 */
+	private function assertRevisionContentModel( Title $title ) {
+		$this->convertRevisionContentModel( $title );
+	}
+
+	/**
+	 * @param Title $title
+	 */
+	private function convertRevisionContentModel( Title $title ) {
+		$toConvert = $this->getDB( DB_REPLICA )->select(
+			[ 'r' => 'revision', 's' => 'slots', 'c' => 'content' ],
+			[ 'rev_id', 'content_id' ],
+			[
+				'rev_page' => $title->getArticleID(),
+				'content_model != ' . $this->bookContentModelId
+			],
+			__METHOD__,
+			[],
+			[
+				's' => [ 'INNER JOIN', 'r.rev_id = s.slot_revision_id' ],
+				'c' => [ 'INNER JOIN', 's.slot_content_id = c.content_id' ]
+			]
+		);
+		if ( !$toConvert->numRows() ) {
+			return;
+		}
+		$ids = [];
+		foreach ( $toConvert as $row ) {
+			$ids[] = $row->content_id;
+		}
+		$db = $this->getDB( DB_PRIMARY );
+		$db->update(
+			'content',
+			[ 'content_model' => $this->bookContentModelId ],
+			[ 'content_id IN (' . $db->makeList( $ids ) . ')' ],
+			__METHOD__
+		);
+	}
+
+	/**
+	 * @param bool $hadCreated
+	 * @return int
+	 * @throws MWException
+	 */
+	private function getBookContentModelId( bool $hadCreated = false ) {
+		$res = $this->getDB( DB_REPLICA )->selectRow(
+			'content_models',
+			[ 'model_id' ],
+			[ 'model_name' => 'book' ],
+			__METHOD__
+		);
+		if ( !$res ) {
+			if ( $hadCreated ) {
+				throw new MWException( "Content model 'book' not found and couldnt be created" );
+			}
+			$this->getDB( DB_PRIMARY )->insert(
+				'content_models',
+				[ 'model_name' => 'book' ],
+				__METHOD__
+			);
+			return $this->getBookContentModelId( true );
+		}
+		return (int)$res->model_id;
+	}
+
 }
 
 $maintClass = ConvertContentModel::class;
