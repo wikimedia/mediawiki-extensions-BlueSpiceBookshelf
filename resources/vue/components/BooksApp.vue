2963055899dadd3721b18390a92a225e89b52eed
Mschlindwein <schlindwein@hallowelt.com>
2024-10-31 11:13:43 +0100

    Fix sorting books
    ERM39025
    
    [4.5.x]
    
    Change-Id: Ib3431433b4d92903a0ab379145268ff83b039f9f

--- resources/vue/components/BooksApp.vue
+++ resources/vue/components/BooksApp.vue
@@ -64,10 +64,6 @@ module.exports = exports = {
 		if ( this.items.length > 0 ) {
 			// Initial bookselfs is in local scope. The bookshelfs value in return statement is global.
 			// If we use bookshelfs here for createBookshelfs the search input would not change the visible items.
-			// Sort books alphabetically
-			this.items.sort( function( bookA, bookB ) {
-				return bookA.displaytitle.toLowerCase().localeCompare( bookB.displaytitle.toLowerCase() );
-			} );
 			initialBookshelfs = createBookshelfs( this.items );
 
 			var initialSearchInputValue = '';
