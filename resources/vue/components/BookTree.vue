b8c5acf1f038496c3f0055228e0969c21a064296
Mschlindwein <schlindwein@hallowelt.com>
2024-12-03 15:05:47 +0100

    Fix loading of vuejsplus component
    ERM39839
    
    [5.x]
    
    Depends-On: I30eda77a1c6a59d1c2cc9eb4212c722d13712b05
    
    Change-Id: I7fac2e59494a1184616d61065d4e77c32e13b153
    (cherry picked from commit 896ee864448b1a17eda68a513b4c7cffaf92bc1a)

---
+++ resources/vue/components/BookTree.vue
@@ -0,0 +1,76 @@
+<template>
+	<tree
+		:class="classes"
+		:nodes="nodes"
+		:selectable="selectable"
+		:selected="selected"
+		:expandable="expandable"
+		:expanded="expanded"
+		:tools="tools"
+		:toolbar="toolbar"
+		:toolbarFloatingOffset="toolbarFloatingOffset"
+		></tree>
+</template>
+
+<script>
+	const { defineComponent } = require( 'vue' );
+	const { Tree } = require( './../../vuejsplus.js' );
+
+	module.exports = defineComponent( {
+		name: 'BookTree',
+		components: {
+			Tree
+		},
+		props: {
+			classes: {
+				type: String,
+				default: ''
+			},
+			nodes: {
+				type: Array,
+				default: []
+			},
+			selectable: {
+				type: Boolean,
+				default: false
+			},
+			selected: {
+				type: Boolean,
+				default: false
+			},
+			expandable: {
+				type: Boolean,
+				default: false
+			},
+			expanded: {
+				type: Boolean,
+				default: false
+			},
+			toolbar: {
+				type: Boolean,
+				default: false
+			},
+			tools: {
+				type: Array,
+				default: []
+			},
+			toolbarFloatingOffset: {
+				type: Number,
+				default: 0
+			}
+		},
+		data: function () {
+			return {
+				classes: this.class,
+				nodes: this.nodes,
+				selectable:this.selectable,
+				selected:this.selected,
+				expandable: this.expandable,
+				expanded: this.expanded,
+				tools: this.tools,
+				toolbar:this.toolbar,
+				toolbarFloatingOffset: this.toolbarFloatingOffset
+			};
+		}
+	} );
+</script>
