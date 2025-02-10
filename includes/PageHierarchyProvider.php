0174598fb443e60eac33420d943d7d8626120284
hslater <hamish.slater@hallowelt.com>
2025-02-10 17:53:26 +0100

    Update ConfigException namespace
    ERM38444
    
    [REL1_43, master]

--- includes/PageHierarchyProvider.php
+++ includes/PageHierarchyProvider.php
@@ -2,6 +2,7 @@
 
 use BlueSpice\Bookshelf\ILineProcessor;
 use BlueSpice\ExtensionAttributeBasedRegistry;
+use MediaWiki\Config\ConfigException;
 use MediaWiki\Json\FormatJson;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
