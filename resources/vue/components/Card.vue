80df30b6f8759e4f94501a0238771b638ac1cbce
Mschlindwein <schlindwein@hallowelt.com>
2025-02-19 10:49:26 +0100

    Fix card styling
    ERM40725
    
    [5.x]
    
    Change-Id: I6975ace297d672c6c6d45b0e5eedc479c8ffba6d

--- resources/vue/components/Card.vue
+++ resources/vue/components/Card.vue
@@ -115,10 +115,10 @@ module.exports = {
 <style lang="css">
 .bs-card {
 	position: relative;
-	width: 320px;
+	width: 325px;
 	height: 450px;
 	border: 1px solid #d7d7d7;
-	margin: 20px 26px;
+	margin: 20px 20px;
 }
 .bs-card.new {
 	outline: var(--bs-books-overview-page-book-new) solid 3px;
@@ -163,15 +163,16 @@ module.exports = {
 	position: absolute;
 	bottom: 0;
 	left: 0;
-	height: 47px;
 	width: 100%;
-	padding: 10px 10px 0 10px;
+	padding: 0;
 }
 .bs-card-actions {
 	display: flex;
   	justify-content: space-between;
 	list-style: none;
 	margin: 0;
+	flex-wrap: wrap;
+	padding: 15px 0;
 }
 .bs-card-actions > li {
 	margin: 0;
