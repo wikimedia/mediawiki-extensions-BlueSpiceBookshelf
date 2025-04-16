a75db2b4aafec42ccf91485d74f9d2affb966aa8
hslater <hamish.slater@hallowelt.com>
2025-04-16 16:44:13 +1200

    Add SemanticExtraSpecialProperty 'Books'
    'The books this page is a part of'
    
    ERM41743
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: Ic92e89ecbd7d9cab7fa5367fac8538834bb6c029

--- src/HookHandler/QueueJobs.php
+++ src/HookHandler/QueueJobs.php
@@ -8,8 +8,9 @@ use BS\ExtendedSearch\Source\Job\UpdateWikiPage;
 use JobQueueGroup;
 use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\Title\Title;
+use SMW\MediaWiki\Jobs\UpdateJob;
 
-class UpdateSearchIndex implements BSBookshelfPageAddedToBookHook, BSBookshelfPageRemovedFromBookHook {
+class QueueJobs implements BSBookshelfPageAddedToBookHook, BSBookshelfPageRemovedFromBookHook {
 
 	/** @var JobQueueGroup */
 	private $jobQueueGroup;
@@ -25,19 +26,23 @@ class UpdateSearchIndex implements BSBookshelfPageAddedToBookHook, BSBookshelfPa
 	 * @inheritDoc
 	 */
 	public function onBSBookshelfPageAddedToBook( Title $book, Title $page ): void {
-		if ( !ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceExtendedSearch' ) ) {
-			return;
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceExtendedSearch' ) ) {
+			$this->jobQueueGroup->push( new UpdateWikiPage( $page ) );
 		}
-		$this->jobQueueGroup->push( new UpdateWikiPage( $page ) );
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceSMWConnector' ) ) {
+			$this->jobQueueGroup->push( new UpdateJob( $page ) );
+		}
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function onBSBookshelfPageRemovedFromBook( Title $book, Title $page ): void {
-		if ( !ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceExtendedSearch' ) ) {
-			return;
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceExtendedSearch' ) ) {
+			$this->jobQueueGroup->push( new UpdateWikiPage( $page ) );
 		}
-		$this->jobQueueGroup->push( new UpdateWikiPage( $page ) );
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'BlueSpiceSMWConnector' ) ) {
+			$this->jobQueueGroup->push( new UpdateJob( $page ) );
+		}
 	}
 }
