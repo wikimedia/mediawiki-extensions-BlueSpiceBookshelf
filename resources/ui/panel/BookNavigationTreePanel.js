f5acbfd8e5e48105572c56bef383e5a39aab7d13
Mschlindwein <schlindwein@hallowelt.com>
2025-05-14 15:18:46 +0200

    Fix expand paths set for bookshelf navigation
    ERM42176
    
    [5.1.x]
    
    Change-Id: I38ff7acf82399accfecf05d5ff8b7e5feada0fd2
    (cherry picked from commit acf5e1e20e47ac7acf8e51d5a82b7e563709f2a6)

--- resources/ui/panel/BookNavigationTreePanel.js
+++ resources/ui/panel/BookNavigationTreePanel.js
@@ -12,6 +12,28 @@ ext.bookshelf.ui.panel.BookNavigationTreePanel = function ( cfg ) {
 
 OO.inheritClass( ext.bookshelf.ui.panel.BookNavigationTreePanel, OOJSPlus.ui.panel.NavigationTreePanel );
 
+ext.bookshelf.ui.panel.BookNavigationTreePanel.prototype.setupTree = function () {
+	this.$treeCnt = $( '<div>' ).addClass(
+		'oojsplus-panel-nav-tree-cnt' );
+	this.$element.append( this.$treeCnt );
+
+	const pageName = mw.config.get( 'wgPageName' );
+	const ns = mw.config.get( 'wgCanonicalNamespace' );
+
+	let root = pageName;
+	if ( ns === '' ) {
+		root = ':' + pageName;
+	}
+
+	this.store.getExpandedPath( root, [ root ] ).done( ( data ) => {
+		this.pages = data;
+		if ( $( document ).find( '#' + this.skeletonID ) ) {
+			$( '#' + this.skeletonID ).empty();
+		}
+		this.updatePages();
+	} );
+};
+
 ext.bookshelf.ui.panel.BookNavigationTreePanel.prototype.updatePages = function () {
 	this.$treeCnt.children().remove();
 
