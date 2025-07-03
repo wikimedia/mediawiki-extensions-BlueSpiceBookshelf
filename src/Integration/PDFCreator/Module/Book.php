7ed9d5f083302c2265ae6b15add0035c6aecb156
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-07-03 11:51:26 +0200

    Improve pdf accessibility
    [5.2]
    
    ERM41785
    
    Depends-on: Ib8fb6ef0ceda97be07e46011daae9b2fc5786685
    Change-Id: I350fa29cff01b4da6b77b8fa03c5510ac5a2aff6

--- src/Integration/PDFCreator/Module/Book.php
+++ src/Integration/PDFCreator/Module/Book.php
@@ -2,7 +2,6 @@
 
 namespace BlueSpice\Bookshelf\Integration\PDFCreator\Module;
 
-use BlueSpice\Bookshelf\Integration\PDFCreator\Utility\BookmarksXMLBuilder;
 use BlueSpice\Bookshelf\Integration\PDFCreator\Utility\TocBuilder;
 use MediaWiki\Extension\PDFCreator\Module\Batch;
 use MediaWiki\Extension\PDFCreator\Utility\ExportContext;
@@ -33,15 +32,5 @@ class Book extends Batch {
 		] );
 		$page = $this->exportPageFactory->getPageFromSpec( $pageSpec, $this->template, $context, $this->workspace );
 		array_unshift( $pages, $page );
-	}
-
-	/**
-	 * @param array $pages
-	 * @param ExportContext $context
-	 * @return string
-	 */
-	protected function getBookmarksXML( array $pages, ExportContext $context ): string {
-		$xmlBuilder = new BookmarksXMLBuilder();
-		return $xmlBuilder->execute( $pages );
 	}
 }
