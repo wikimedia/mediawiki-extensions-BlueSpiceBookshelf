e219a9961d029ccbffd3c95656cebf674299f89f
hslater <hamish.slater@hallowelt.com>
2025-10-07 14:39:52 +0700

    CI fixes & WikiTextLink handleRedirect()
    RestStructureTest
    Defensive checks
    Add missing BookshelfSupressBookNS config
    BookSourceParserTest
    WikiTextLinkTest
    Add missing wikitext component dependency
    
    ERM20742
    
    [5.1.x]
    
    Change-Id: I35833a4306c454fe31d37ff0ef34fa62f2e29822

--- tests/phpunit/BookSourceParserTest.php
+++ tests/phpunit/BookSourceParserTest.php
@@ -4,8 +4,8 @@ namespace BlueSpice\Bookshelf\Tests;
 
 use BlueSpice\Bookshelf\BookSourceParser;
 use BlueSpice\Bookshelf\ChapterDataModel;
-use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
+use MWStake\MediaWiki\Component\Wikitext\ParserFactory;
 
 /**
  * @group BlueSpice
@@ -30,30 +30,25 @@ class BookSourceParserTest extends MediaWikiIntegrationTestCase {
 ** [[B/A]]
 HERE;
 
-	public function addDBDataOnce(): void {
-		$this->insertPage(
-			$this->getBookTitle()->getPrefixedText(),
-			$this->dummyBookContent,
-		);
-	}
-
 	/**
 	 * @covers \BlueSpice\Bookshelf\BookSourceParser::getChapterDataModelArray
 	 */
 	public function testGetChapterDataModelArray() {
+		$title = $this->insertPage( 'BookPage', $this->dummyBookContent )['title'];
+
 		$expected = $this->getExpecedOutput();
 
 		$services = $this->getServiceContainer();
 		$revisionLookup = $services->getRevisionLookup();
+		/** @var ParserFactory */
 		$parserFactory = $services->get( 'MWStakeWikitextParserFactory' );
+		$nodeProcessors = $parserFactory->getNodeProcessors();
 		$titleFactory = $services->getTitleFactory();
-		$revisionRecord = $revisionLookup->getRevisionByTitle(
-			$this->getBookTitle()
-		);
+		$revisionRecord = $revisionLookup->getRevisionByTitle( $title );
 
 		$parser = new BookSourceParser(
 			$revisionRecord,
-			$parserFactory->getNodeProcessors(),
+			$nodeProcessors,
 			$titleFactory
 		);
 
@@ -64,23 +59,20 @@ HERE;
 
 	private function getExpecedOutput(): array {
 		$data = [
-			new ChapterDataModel( null, null, 'A', '1', 'plain-text' ),
-			new ChapterDataModel( 0, 'A/A', 'aa', '1.1', 'wikilink-with-alias' ),
-			new ChapterDataModel( 0, 'A/A/A', 'aaa', '1.1.1', 'wikilink-with-alias' ),
-			new ChapterDataModel( 0, 'A/A/B', 'aab', '1.1.2', 'wikilink-with-alias' ),
-			new ChapterDataModel( 0, 'A/A/B/A', 'aaba', '1.1.2.1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A', 'A', '1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A/A', 'aa', '1.1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A/A/A', 'aaa', '1.1.1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A/A/B', 'aab', '1.1.2', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A/A/B/A', 'aaba', '1.1.2.1', 'wikilink-with-alias' ),
 			new ChapterDataModel( null, null, 'Hello', '2', 'plain-text' ),
 			new ChapterDataModel( null, null, 'World', '2.1', 'plain-text' ),
-			new ChapterDataModel( 0, 'A/B', 'ab', '2.2', 'wikilink-with-alias' ),
-			new ChapterDataModel( 10, 'A/B/A', 'aba', '2.2.1', 'wikilink-with-alias' ),
-			new ChapterDataModel( 0, 'B', 'B', '3', 'wikilink-with-alias' ),
-			new ChapterDataModel( 0, 'B/A', 'B/A', '3.1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'A/B', 'ab', '2.2', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_TEMPLATE, 'A/B/A', 'aba', '2.2.1', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'B', 'B', '3', 'wikilink-with-alias' ),
+			new ChapterDataModel( NS_MAIN, 'B/A', 'B/A', '3.1', 'wikilink-with-alias' ),
 		];
 
 		return $data;
 	}
 
-	private function getBookTitle(): Title {
-		return Title::makeTitle( NS_BOOK, 'UNIT TEST BOOK' );
-	}
 }
