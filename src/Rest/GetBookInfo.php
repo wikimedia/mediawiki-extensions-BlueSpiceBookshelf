39c5cc885dd59651ec4c26ce3d15d96f679be581
d.savuljesku <d.savuljesku@gmail.com>
2026-05-26 15:16:39 +0200

    Prevent overwriting book content when adding a page to a book
    [5.1.x]
    
    ERM46316
    
    Change-Id: I884958399c9f849a6dc36102b6487829c5317e1f

---
+++ src/Rest/GetBookInfo.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace BlueSpice\Bookshelf\Rest;
+
+use BlueSpice\Bookshelf\BookLookup;
+use BlueSpice\Bookshelf\BookMetaLookup;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFactory;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class GetBookInfo extends SimpleHandler {
+
+	/**
+	 *
+	 * @param TitleFactory $titleFactory
+	 * @param BookLookup $bookLookup
+	 * @param BookMetaLookup $bookMetaLookup
+	 */
+	public function __construct(
+		private readonly TitleFactory $titleFactory,
+		private readonly BookLookup $bookLookup,
+		private readonly BookMetaLookup $bookMetaLookup
+	) {
+	}
+
+	/**
+	 * @return Response
+	 */
+	public function run() {
+		$bookTitle = $this->getBookTitle();
+		if ( !$bookTitle ) {
+			return $this->getResponseFactory()->createHttpError( 404, [ 'No valid book title' ] );
+		}
+		$info = $this->bookLookup->getBookInfo( $bookTitle );
+		if ( !$info ) {
+			return $this->getResponseFactory()->createHttpError( 404, [ 'Book not found' ] );
+		}
+		return $this->getResponseFactory()->createJson( array_merge(
+			$info->jsonSerialize(),
+			[ 'meta' => $this->getBookMeta( $bookTitle ) ]
+		) );
+	}
+
+	/**
+	 * @return Title|null
+	 */
+	protected function getBookTitle(): ?Title {
+		$validated = $this->getValidatedParams();
+		$bookName = $validated[ 'booktitle' ];
+		return $this->titleFactory->newFromText( $bookName );
+	}
+
+	/**
+	 * @param Title $bookTitle
+	 * @return array
+	 */
+	protected function getBookMeta( Title $bookTitle ): array {
+		return $this->bookMetaLookup->getMetaForBook( $bookTitle );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getParamSettings() {
+		return [
+			'booktitle' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			]
+		];
+	}
+}
