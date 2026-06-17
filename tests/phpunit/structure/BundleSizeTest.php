363d17a033167a31653c23ef5b127e2bed30b5be
Hamish <hamish.slater@hallowelt.com>
2026-06-18 00:40:46 +0700

    Mock BookMetaLookup in BundleSizeTest
    Prevents Error: Call to a member function field() on null
    
    ERM20742
    
    [5.1.x]
    
    Depends-On: I828c7f6010fb18b4c3b239d471aca974ea6e0cf2
    Change-Id: Ifc365e6344ff3bd07f32ac3fe1105b0736c3afba
    (cherry picked from commit 513ed12dcbe3d3950fa46447d3a323a795557e67)

--- tests/phpunit/structure/BundleSizeTest.php
+++ tests/phpunit/structure/BundleSizeTest.php
@@ -1,9 +1,17 @@
 <?php
 namespace BlueSpice\Bookshelf\Tests\Structure;
 
+use BlueSpice\Bookshelf\BookMetaLookup;
 use MediaWiki\Tests\Structure\BundleSizeTestBase;
 
 class BundleSizeTest extends BundleSizeTestBase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		$metaLookup = $this->createMock( BookMetaLookup::class );
+		$metaLookup->method( 'getAllMetaValuesForKey' )->willReturn( [] );
+		$this->setService( 'BSBookshelfBookMetaLookup', $metaLookup );
+	}
 
 	/** @inheritDoc */
 	public function getBundleSizeConfig(): string {
